b7356539fb018101182bd19737a1131fd116456e
kurooru <kurooru98@gmail.com>
2024-06-08 19:33:47 +0900

    Title: 단어로 구분하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240608/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#define TEN 10
+
+int main() {
+    string s;
+    for (int i = 0; i < TEN; i++) {
+        cin >> s;
+        cout << s << endl;
+    }    
+    return 0;
+}
