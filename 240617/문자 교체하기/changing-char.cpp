0c53ae15b6e8d7a5d934b1b7521f966b0dca834c
kurooru <kurooru98@gmail.com>
2024-06-17 19:47:42 +0900

    Title: 문자 교체하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240617/문자 교체하기/changing-char.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // input
+    string str1, str2;
+    cin >> str1 >> str2;
+
+    for (int i = 0; i < 2; i++) {
+        str2[i] = str1[i];
+    }
+
+    cout << str2;
+
+    return 0;
+}
