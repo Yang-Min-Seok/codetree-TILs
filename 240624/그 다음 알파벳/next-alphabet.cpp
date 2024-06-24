5d8d7033122506dd4eaf203b71b028e91a41e364
kurooru <kurooru98@gmail.com>
2024-06-24 19:46:05 +0900

    Title: 그 다음 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240624/그 다음 알파벳/next-alphabet.cpp
+++ 240624/그 다음 알파벳/next-alphabet.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main() {
     char c;
     cin >> c;
-    cout << (char) (c + 1);
+    if (c != 'z') {
+        cout << (char) ((int) (c + 1));
+    } else {
+        cout << 'a';
+    }
     return 0;
 }
