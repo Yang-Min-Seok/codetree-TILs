122e7e67ea054b24a696479afa83127b1ea2f1a4
kurooru <kurooru98@gmail.com>
2024-06-24 19:51:05 +0900

    Title: 그 전 알파벳, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240624/그 전 알파벳/before-the-alphabet.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+    if (c == 'a') {
+        cout << 'z';
+    } else {
+        cout << (char) ((int) (c - 1));
+    }
+    return 0;
+}
